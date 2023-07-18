e7338472d02e8a901eb90c4e126032993ebfe703
DmitrySnTDP <89049713204s@gmail.com>
2023-07-18 17:22:13 +0500

    hw_5_n4

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',index,name='main-page'),
     path('lesson4/',lessonFour),
     path('top-sellers/', top_sellers, name = 'top-sellers'),
+    path('index/',index, name='index'),
 ]
