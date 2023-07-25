580509a82dfe7a6c59d1ca9301388b3a4dc214ea
DmitriySnTDP <89049713204s@gmail.com>
2023-07-25 18:29:21 +0500

    lesson7_3

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
     description = models.TextField("описание")
-    price = models.DecimalField("цена", max_length=10, decimal_places=2)
+    price = models.DecimalField("цена",max_digits=10, decimal_places=2)
     auction = models.BooleanField("торг", help_text = "Отметьте если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now = True)
