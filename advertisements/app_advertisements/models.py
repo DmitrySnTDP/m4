f445cd2ec26631d2b0c7115b6d904b86469d1675
DmitriySnTDP <89049713204s@gmail.com>
2023-07-25 17:59:43 +0500

    lesson7_2

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -7,3 +7,4 @@ class Advertisement(models.Model):
     price = models.DecimalField("цена", max_length=10, decimal_places=2)
     auction = models.BooleanField("торг", help_text = "Отметьте если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now = True)
