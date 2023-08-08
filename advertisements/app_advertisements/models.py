5b01431759395686470fb5c7cf9c21873581309e
DmitriySnTDP <89049713204s@gmail.com>
2023-08-08 17:17:56 +0500

    hw7_n4

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -8,3 +8,9 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text = "Отметьте если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = 'advertisements'
