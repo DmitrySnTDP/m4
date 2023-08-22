385a6ac8a6a425cc4bfe43e0eec9ca1c0e97fb95
DmitriySnTDP <89049713204s@gmail.com>
2023-08-22 17:48:46 +0500

    less9

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -3,12 +3,12 @@ from .models import Advertisement
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction', 'updated_date']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction', 'updated_date','image']
     list_filter = ['auction', 'created_at']
     actions = ['action_auction_as_false', 'action_auction_as_true']
     fieldsets = (
         ('общее', {
-            'fields':('title','description')
+            'fields':('title','description','image')
         }),
         ("Финансы", {
             'fields':('price','auction'),
--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,10 +1,9 @@
-from .views import index, lessonFour, top_sellers
+from .views import index, top_sellers
 from django.urls import path
 
 
 urlpatterns = [
     path('',index,name='main-page'),
-    path('lesson4/',lessonFour),
     path('top-sellers/', top_sellers, name = 'top-sellers'),
     path('index/',index, name='index'),
 ]
